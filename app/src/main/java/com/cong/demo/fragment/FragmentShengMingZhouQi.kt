d5b8a63f9c150e5fb7e13f201d0caabc242d808a
席聪 <user.cong@gmail.com>
2020-04-09 20:01:00 +0800

    ‘每次改完的例行提交’

---
+++ app/src/main/java/com/cong/demo/fragment/FragmentShengMingZhouQi.kt
@@ -0,0 +1,13 @@
+package com.cong.demo.fragment
+
+import android.os.Bundle
+import com.cong.demo.BaseActivity
+
+class FragmentShengMingZhouQi : BaseActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.)
+    }
+    
+}
---
+++ app/src/main/java/com/cong/demo/fragment/Wd.md
@@ -0,0 +1,3 @@
+fragment add 和 replace 的区别
+onhide  ishide 什么时候true
+string stringbuffer stringbuild区别
